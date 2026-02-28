60937630f4112bf2aeab45a336940e04d7a0097d
Manish Kumar <kumarmanish85211@gmail.com>
2026-02-28 16:04:05 +0530

    check

--- core/email_service.py
+++ core/email_service.py
@@ -5,8 +5,8 @@ import random
 import time
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
-SENDER_EMAIL = "glimpsefilmy@gmail.com"          # Put your real email here
-SENDER_PASSWORD = "bocwoewklavlnzkt"
+SENDER_EMAIL = "EMAIL HERE"          # Put your real email here
+SENDER_PASSWORD = "PASS HERE"
 class EmailService:
     def __init__(self):
         # We will load these from your config or hardcode for testing
@@ -14,8 +14,8 @@ class EmailService:
         self.smtp_port = 587
         
         # TODO: Replace with your Gmail and the 16-letter App Password you generated
-        self.sender_email = "glimpsefilmy@gmail.com" 
-        self.sender_password = "bocwoewklavlnzkt" 
+        self.sender_email = "EMAILHERE" 
+        self.sender_password = "PASS HERE" 
         
         # Memory to store OTPs temporarily (Email -> {otp, timestamp})
         self.active_otps = {}
